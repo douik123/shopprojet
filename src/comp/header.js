23f44034c031c1ccb96286124ca4c5de8d2dd9ae
douik123 <ahmed@predict-a.com>
2022-08-03 14:31:56 +0200

    first Components

---
+++ src/comp/header.js
@@ -0,0 +1,35 @@
+import { AiFillShopping } from "react-icons/ai";
+import { ImLocation2 } from "react-icons/im";
+import { AiOutlineSearch } from "react-icons/ai";
+import { AiFillHeart } from "react-icons/ai";
+import { AiOutlineUser } from "react-icons/ai";
+function App() {
+  return (
+    <div className="header">
+ <div className="iconshop"><h3><AiFillShopping/>DouikStore</h3></div>
+ <div className="icon">
+    <div>
+    <ImLocation2/>
+    </div>
+    <div className='location'>
+      <div><h6>Select your location</h6></div>
+    </div>
+ </div>
+ <div className="search">
+    <div><input  type="text" placeholder="Search For Products" size="49"></input></div>
+    <div className="iconsear"><AiOutlineSearch/></div>
+</div>
+<div>
+  <div className='icons'>
+    <div className='heart'><AiFillHeart/></div>
+    <div className='user'><AiOutlineUser/></div>
+    <div className='shopp'><AiFillShopping/></div>
+  </div>
+</div>
+
+     
+    </div>
+  );
+}
+
+export default App;
